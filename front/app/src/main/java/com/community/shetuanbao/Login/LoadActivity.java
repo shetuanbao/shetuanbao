1f5f6c99baee26c8730b0f0628bcb8c7bc354c3a
koori0829 <1079414416@qq.com>
2019-04-24 15:50:36 +0800

    app载入

---
+++ front/app/src/main/java/com/community/shetuanbao/Login/LoadActivity.java
@@ -0,0 +1,15 @@
+package com.community.shetuanbao.Login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.community.shetuanbao.R;
+
+public class LoadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
