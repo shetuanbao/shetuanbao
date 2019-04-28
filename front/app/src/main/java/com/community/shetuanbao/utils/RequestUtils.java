7caffd11f36ce42fb4d6b153de85d20e401efa9c
koori0829 <1079414416@qq.com>
2019-04-28 14:32:47 +0800

    处理get和post请求

---
+++ front/app/src/main/java/com/community/shetuanbao/utils/RequestUtils.java
@@ -0,0 +1,62 @@
+package com.community.shetuanbao.utils;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/*
+ * 请求工具类，封装了post和get请求
+ */
+public class RequestUtils {
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    public static final String HOSTPORT = "http://192.168.62.1:8010"; //后端端口号已设置为8010无需修改，ip地址改成当前的ipv4地址
+
+    private static OkHttpClient client = new OkHttpClient();
+
+    // POST请求
+    public static String post(String url, Map<String, Object> params) throws IOException {
+
+        // 拼接url
+        url = HOSTPORT + url;
+
+        // 将请求参数变成JSON字符串
+        JSONObject json = new JSONObject(params);
+        String jsonString = json.toString();
+
+        Log.d("request", "url: " + url + ", params: " + jsonString);
+
+        // 创建请求体和请求对象
+        RequestBody body = RequestBody.create(JSON, jsonString);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        // 获取响应并返回
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static String get(String url) throws IOException {
+        // 拼接url
+        url = HOSTPORT + url;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+}
