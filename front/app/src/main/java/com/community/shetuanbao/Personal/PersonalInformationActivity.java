2ad548d3029dd33ab2a5bb9d9d5e3a4b44937d08
koori0829 <1079414416@qq.com>
2019-05-03 23:59:33 +0800

    个人资料activity

---
+++ front/app/src/main/java/com/community/shetuanbao/Personal/PersonalInformationActivity.java
@@ -0,0 +1,15 @@
+package com.community.shetuanbao.Personal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.community.shetuanbao.R;
+
+public class PersonalInformationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_information);
+    }
+}
